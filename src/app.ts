d77c8fb8f39dbb2be3b45fc11c75fed0799cc4f0
deny <d3nys.pavlov@gmail.com>
2024-12-09 23:22:07 +0200

    refactor: update CORS policy to allow localhost:5173

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true
   })
 );
