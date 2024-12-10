22f735274c2a633a7194c9dcd77efeca848eccd1
deny <d3nys.pavlov@gmail.com>
2024-12-10 20:15:55 +0200

    fix: update cookie sameSite attribute to 'none'

--- src/config/token.ts
+++ src/config/token.ts
@@ -9,7 +9,7 @@ export const generateToken = (id: string, res: Response) => {
   res.cookie('jwt', token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: 'none',
     secure: process.env.NODE_ENV !== 'development'
   });
 
