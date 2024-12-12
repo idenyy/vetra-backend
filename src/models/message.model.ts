c60150d67ed7d2e286ee9fa455b9b70039a2f0f3
deny <d3nys.pavlov@gmail.com>
2024-12-13 00:19:51 +0200

    fix: change foreignKey from userId to senderId

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -46,7 +46,7 @@ Message.init(
 
 User.hasMany(Message, { foreignKey: 'senderId', as: 'sentMessages' });
 User.hasMany(Message, { foreignKey: 'receiverId', as: 'receivedMessages' });
-Message.belongsTo(User, { foreignKey: 'userId', as: 'sender' });
+Message.belongsTo(User, { foreignKey: 'senderId', as: 'sender' });
 Message.belongsTo(User, { foreignKey: 'receiverId', as: 'receiver' });
 
 export default Message;
