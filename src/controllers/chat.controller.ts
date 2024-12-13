0d1b7fad7cba7bacaa52a61b1bab720b9e1eada2
deny <d3nys.pavlov@gmail.com>
2024-12-13 13:50:06 +0200

    remove: messages check in getMessages

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -27,8 +27,6 @@ export const getMessages = async (req: Request, res: Response): Promise<any> => 
       ]
     });
 
-    if (!messages.length) return res.status(404).json({ error: 'No Messages Found' });
-
     res.status(200).json(messages);
   } catch (error: any) {
     console.error(`Error in [getMessages] controller: ${error.message}`);
