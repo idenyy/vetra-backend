2f8b498c5b56c95fe030a57dd00da1b8b0b0fe1a
deny <d3nys.pavlov@gmail.com>
2024-12-13 14:06:58 +0200

    feat: add timestamps

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -1,25 +1,28 @@
 import mongoose from 'mongoose';
 
-const MessageSchema = new mongoose.Schema({
-  senderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const MessageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    content: {
+      type: JSON,
+      required: true
+    },
+    isRead: {
+      type: Boolean,
+      default: false
+    }
   },
-  receiverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  content: {
-    type: JSON,
-    required: true
-  },
-  isRead: {
-    type: Boolean,
-    default: false
-  }
-});
+  { timestamps: true }
+);
 
 const Message = mongoose.model('Message', MessageSchema);
 
