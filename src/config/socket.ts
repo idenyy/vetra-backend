bf6357b023abf48cd47c26b62b137ec968e37457
deny <d3nys.pavlov@gmail.com>
2024-12-13 21:47:03 +0200

    feat: add socket io

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -12,11 +12,20 @@ const io = new Server(server, {
   }
 });
 
+const userSocketMap: any = {} as any;
+
 io.on('connection', (socket) => {
   console.log('User connected: ', socket.id);
 
+  const userId = socket.handshake.query.userId as string;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
   socket.on('disconnect', () => {
     console.log('User disconnected: ', socket.id);
+    delete userSocketMap[userId];
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
   });
 });
 
