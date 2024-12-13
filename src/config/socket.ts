3487cf1165ebe4ed2f53d3ea3e24150411f18013
deny <d3nys.pavlov@gmail.com>
2024-12-13 23:36:28 +0200

    refactor: cors origin

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -8,7 +8,7 @@ export const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true
   }
 });
