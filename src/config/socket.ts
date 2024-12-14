cea4d05c271a42057b159cd8e55adf25c452a92d
Pavlov Denys <116432682+idenyy@users.noreply.github.com>
2024-12-15 00:39:11 +0200

    Update socket.ts

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -8,7 +8,7 @@ export const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://vetra-pi.vercel.app', 'https://vetra-deny-dev.vercel.app', 'https://vetra-ep3jct8gw-deny-dev.vercel.app'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://vetra-pi.vercel.app', 'https://vetra-deny-dev.vercel.app', 'https://vetra-psctoh42b-deny-dev.vercel.app/'],
     credentials: true
   }
 });
